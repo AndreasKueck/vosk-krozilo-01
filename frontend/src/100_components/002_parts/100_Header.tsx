220188666581bed0c73580dd25b726fa0171561b
AndreasKueck <ak163@gmx.net>
2023-02-10 09:06:04 +0100

    Update 100_Header.tsx

--- frontend/src/100_components/002_parts/100_Header.tsx
+++ frontend/src/100_components/002_parts/100_Header.tsx
@@ -6,11 +6,11 @@ export const Header = () => {
         <div className="header">
             <div className="header-application-title-container">
                 <img src="./assets/icons/flect.png" className="header-application-title-logo"></img>
-                <div className="header-application-title-text">Offline Transcribe</div>
+                <div className="header-application-title-text">Eksterrete transskribi</div>
             </div>
 
             <div className="header-button-container">
-                <a className="header-button-link" href="https://github.com/w-okada/vosk-browser-ts" target="_blank" rel="noopener noreferrer">
+                <a className="header-button-link" href="https://github.com/AndreasKueck/vosk-krozilo-01" target="_blank" rel="noopener noreferrer">
                     <img src="./assets/icons/github.svg" />
                 </a>
                 <div className="header-button-spacer"></div>
